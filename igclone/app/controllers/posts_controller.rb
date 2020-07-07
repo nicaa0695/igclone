12164d074e0e8b12b6689447702e75b8deac1a89
nicaa0695 <nicaa0695@gmail.com>
2020-07-07 13:59:06 -0600

    added show, new, and create methods in posts_controller as well as strong params

--- igclone/app/controllers/posts_controller.rb
+++ igclone/app/controllers/posts_controller.rb
@@ -1,3 +1,17 @@
 class PostsController < ApplicationController
 
+    def show
+    end
+      
+    def new
+    end
+      
+    def create
+    end
+      
+    private
+      
+    def post_params
+        params.require(:post).permit(:image)
+    end
 end

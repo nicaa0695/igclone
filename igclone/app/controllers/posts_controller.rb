7691048d92242b449f0c366042863d23c2e452b2
nicaa0695 <nicaa0695@gmail.com>
2020-07-09 14:26:38 -0600

    added submit button for new posts, minor styling and image cache to post parms

--- igclone/app/controllers/posts_controller.rb
+++ igclone/app/controllers/posts_controller.rb
@@ -14,6 +14,6 @@ class PostsController < ApplicationController
     private
       
     def post_params
-        params.require(:post).permit(:image)
+        params.require(:post).permit(:image, :image_cache)
     end
 end
